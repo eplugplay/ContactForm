683e586d36bcae87e595e80b291f2b23ed5faa2e
Steve Yi <steveyi32@gmail.com>
2015-04-02 16:15:02 -0500

    finished.

--- ContactForm/Controllers/HomeController.cs
+++ ContactForm/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using ContactForm;
+using System.Web.Security;
+using System.Text;
+using System.Data;
+using System.Web.Script.Serialization;
 using System.IO;
-using System.Collections;
+using System.Configuration;
+using System.Net;
 using ContactForm.Models;
-using System.Web.Services;
 
 namespace ContactForm.Controllers
 {
@@ -23,7 +26,7 @@ namespace ContactForm.Controllers
 
         [HttpPost]
         [ValidateInput(false)]
-        public ActionResult PostForm(string ContactName, string CompanyName, string Address, string City, string State, string Zip, string Phone, string Email, string Request, string Captcha)
+        public ActionResult PostForm(string ContactName, string CompanyName, string Address, string City, string State, string Zip, string Phone, string Email, string RequestText, string Captcha)
         {
             if (ModelState.IsValid)
             {
@@ -34,19 +37,34 @@ namespace ContactForm.Controllers
                 }
                 else
                 {
-                    string Msg = BuildMsg(ContactName, CompanyName, Address, City, State, Zip, Phone, Email, Request);
+                    string Msg = BuildMsg(ContactName, CompanyName, Address, City, State, Zip, Phone, Email, RequestText);
                     string CustomerMsg = BuildCustomerMsg();
                     bool EmailSent = false;
+                    Stream[] stream = new Stream[Request.Files.Count]; 
                     try
                     {
+                        string[] fileName = new string[Request.Files.Count];
+                        int count = 0;
+                        foreach (string file in Request.Files)
+                        {
+                            var fileContent = Request.Files[file];
+                            if (fileContent != null && fileContent.ContentLength > 0)
+                            {
+                                fileName[count] = fileContent.FileName;
+                                string[] split = fileName[count].Split('\\');
+                                fileName[count] = split[split.Length - 1];
+                                stream[count] = fileContent.InputStream;
+                                count++;
+                            }
+                        }
                         // send email
-                        EmailSent = Classes.Email.SendEmail("chae.song20@gmail.com", "Rhinestone Empire - Contact Form", Msg);
-                        //EmailSent = Classes.Email.SendEmail("steve.yi@gmail.com", "Rhinestone Empire - Contact Form", Msg);
+                        EmailSent = Classes.Email.SendEmail("chae.song20@gmail.com", "Rhinestone Empire - Contact Form", Msg, stream, fileName, false);
+                        //EmailSent = Classes.Email.SendEmail("steve.yi@gmail.com", "Rhinestone Empire - Contact Form", Msg, stream, fileName, false);
                         if (EmailSent == true)
                         {
                             try
                             {
-                                EmailSent = Classes.Email.SendEmail(Email, "Rhinestone Empire - Contact Form", CustomerMsg);
+                                EmailSent = Classes.Email.SendEmail(Email, "Rhinestone Empire - Contact Form", CustomerMsg, stream, fileName, true);
                             }
                             catch
                             {
@@ -69,10 +87,10 @@ namespace ContactForm.Controllers
             return View("Index");
         }
 
-        public string BuildMsg(string ContactName, string CompanyName, string Address, string City, string State, string Zip, string Phone, string Email, string Request)
+        public string BuildMsg(string ContactName, string CompanyName, string Address, string City, string State, string Zip, string Phone, string Email, string RequestText)
         {
             string ToReturn = "<br/>Contact Name: " + ContactName + "<br/>" + "Company Name: " + CompanyName + "<br/>" + "Address: " + Address + "<br/>" +
-                   "City: " + City + "<br/>" + "State: " + State + "<br/>" + "Zip: " + Zip + "<br/>" + "Phone: " + Phone + "<br/>" + "Email: " + Email + "<br/>" + "Request: " + Request + "<br/>";
+                   "City: " + City + "<br/>" + "State: " + State + "<br/>" + "Zip: " + Zip + "<br/>" + "Phone: " + Phone + "<br/>" + "Email: " + Email + "<br/>" + "Request: " + RequestText + "<br/>";
             return ToReturn;
         }
 
--- ContactForm/Models/Email.cs
+++ ContactForm/Models/Email.cs
@@ -5,13 +5,14 @@ using System.Web;
 using System.Net.Mail;
 using System.Data;
 using System.Collections;
+using System.IO;
 
 namespace ContactForm.Classes
 {
     public static class Email
     {
         //send email with attachment
-        public static bool SendEmail(string emailToSend, string subjectToSend, string MsgBody)
+        public static bool SendEmail(string emailToSend, string subjectToSend, string MsgBody, Stream[] files, string[] fileNames, bool isCustomer)
         {
             MailMessage mail = new MailMessage();
             mail.IsBodyHtml = true;
@@ -25,9 +26,24 @@ namespace ContactForm.Classes
             SmtpServer.EnableSsl = true;
             System.Net.NetworkCredential aCredential = new System.Net.NetworkCredential("steveyi32@gmail.com", "");
             SmtpServer.Credentials = aCredential;
+
+            if (!isCustomer)
+            {
+                for (int i = 0; i < files.Length; i++)
+                {
+                    Attachment attachment = new Attachment(files[i], fileNames[i]);
+                    attachment.Name = fileNames[i];
+                    mail.Attachments.Add(attachment);
+                }
+            }
             try
             {
                 SmtpServer.Send(mail);
+                if (!isCustomer)
+                {
+                    mail.Attachments.Clear();
+                    mail.Attachments.Dispose();
+                }
                 return true;
             }
             catch
