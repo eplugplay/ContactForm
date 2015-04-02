21911f3539d2e8a4af160cbed9e0fd187f8582f7
Steve Yi <steveyi32@gmail.com>
2015-04-02 09:23:35 -0500

    first commit

---
+++ Backup/ContactForm/Captcha.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="Captcha.ashx.cs" Class="LAUF_Survey_Report.Captcha" %>
---
+++ Backup/ContactForm/Models/ContactForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+
+namespace ContactForm.Models
+{
+    public class ContactForm
+    {
+        [Required]
+        public string ContactName { get; set; }
+        [Required]
+        public string CompanyName { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string State { get; set; }
+        [Required]
+        public string Zip { get; set; }
+        [Required]
+        public string Phone { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string Captcha { get; set; }
+    }
+}
